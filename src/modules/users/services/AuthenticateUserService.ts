6f4c3accb0de24b76bb26a07e0bc74d135e5e6c4
Matheus <matheusaragaofs@gmail.com>
2021-01-30 12:48:53 -0300

    Configurando imports

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,9 +1,12 @@
 import { compare } from 'bcryptjs';
 import { sign } from 'jsonwebtoken';
 import { getRepository } from 'typeorm';
+
+import authConfig from '@config/auth';
+
+import AppError from '@shared/errors/AppError';
+
 import User from '../infra/typeorm/entities/User';
-import authConfig from '../../../config/auth';
-import AppError from '../../../shared/errors/AppError';
 
 interface Request {
   email: string;
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,9 +3,10 @@ import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import routes from './routes';
 import cors from 'cors';
-import uploadConfig from '../../../config/upload';
-import AppError from '../../errors/AppError';
-import '../typeorm';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
+import '@shared/infra/typeorm';
+
 
 const app = express();
 app.use(cors())
