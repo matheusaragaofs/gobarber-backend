d8bf517462e885946b0749839448cf3af3349d8b
Matheus <matheusaragaofs@gmail.com>
2021-02-03 10:38:50 -0300

    mapeando features do sistema

--- src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
+++ src/shared/container/providers/StorageProvider/fakes/FakeStorageProvider.ts
@@ -1,24 +1,19 @@
+import IStorageProvider from '../models/IStorageProvider';
 
-import IStorageProvider from "../models/IStorageProvider";
+class FakeStorageProvider implements IStorageProvider {
+  private storage: string[] = [];
 
-
-class FakeStorageProvider implements IStorageProvider{
-
-  private storage: string[] = []
-
-  public async saveFile(file:string): Promise<string>{
-    this.storage.push(file)
-    return file
-
-
+  public async saveFile(file: string): Promise<string> {
+    this.storage.push(file);
+    return file;
   }
-  public async deleteFile(file:string):Promise<void>{
-    const findIndex = this.storage.findIndex(storageFile => storageFile === file)
-    this.storage.splice(findIndex,1)
 
-
+  public async deleteFile(file: string): Promise<void> {
+    const findIndex = this.storage.findIndex(
+      storageFile => storageFile === file,
+    );
+    this.storage.splice(findIndex, 1);
+  }
 }
-}
 
-
-export default FakeStorageProvider
+export default FakeStorageProvider;
