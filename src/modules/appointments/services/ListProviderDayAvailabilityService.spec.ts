a15ef15f5203e3c52373581ddd49cb7fbf0985f4
Matheus <matheusaragaofs@gmail.com>
2021-02-06 15:07:08 -0300

    Excluindo horários antigos

--- src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.spec.ts
@@ -1,14 +1,13 @@
-// import AppError from '@shared/errors/AppError';
-import 'reflect-metadata';
-import ListProviderDayAvailability from './ListProviderDayAvailabilityService';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
+import ListProviderDayAvailabilityService from './ListProviderDayAvailabilityService';
 
-let listProviderDayAvailability: ListProviderDayAvailability;
 let fakeAppointmentsRepository: FakeAppointmentsRepository;
-describe('ListProviderMonthAvalability', () => {
+let listProviderDayAvailability: ListProviderDayAvailabilityService;
+
+describe('ListProviderDayAvailability', () => {
   beforeEach(() => {
     fakeAppointmentsRepository = new FakeAppointmentsRepository();
-    listProviderDayAvailability = new ListProviderDayAvailability(
+    listProviderDayAvailability = new ListProviderDayAvailabilityService(
       fakeAppointmentsRepository,
     );
   });
@@ -16,26 +15,34 @@ describe('ListProviderMonthAvalability', () => {
   it('should be able to list the day availability from provider', async () => {
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 8, 0, 0),
+      date: new Date(2021, 4, 20, 14, 0, 0),
     });
+
     await fakeAppointmentsRepository.create({
       provider_id: 'user',
-      date: new Date(2020, 4, 20, 10, 0, 0),
+      date: new Date(2021, 4, 20, 15, 0, 0),
+    });
+
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2021, 4, 20, 11).getTime();
     });
 
     const availability = await listProviderDayAvailability.execute({
       provider_id: 'user',
-      year: 2020,
-      day: 20,
+      year: 2021,
       month: 5,
+      day: 20,
     });
 
     expect(availability).toEqual(
       expect.arrayContaining([
         { hour: 8, available: false },
-        { hour: 9, available: true },
+        { hour: 9, available: false },
         { hour: 10, available: false },
-        { hour: 11, available: true },
+        { hour: 13, available: true },
+        { hour: 14, available: false },
+        { hour: 15, available: false },
+        { hour: 16, available: true },
       ]),
     );
   });
--- src/modules/appointments/services/ListProviderDayAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -1,5 +1,5 @@
 import { injectable, inject } from 'tsyringe';
-import { getHours } from 'date-fns';
+import { getHours, isAfter } from 'date-fns';
 
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
@@ -41,14 +41,17 @@ class ListProviderDayAvailabilityService {
       { length: 10 },
       (value, index) => index + hourStart,
     );
+    const currentDate = new Date(Date.now());
+
     const availability = eachHourArray.map(hour => {
       const hasAppointmentInHours = appointments.find(
         appointment => getHours(appointment.date) === hour,
       );
 
+      const compareDate = new Date(year, month - 1, day, hour);
       return {
         hour,
-        available: !hasAppointmentInHours,
+        available: !hasAppointmentInHours && isAfter(compareDate, currentDate), // só vai estar disponível se o agendamento for futuro, depois da hora atual
       };
     });
 
