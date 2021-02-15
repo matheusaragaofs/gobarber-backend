75b76f421c29e1db9fb999d09ecc41b7bdd062c8
Matheus <matheusaragaofs@gmail.com>
2021-02-15 20:37:44 -0300

    serialização no cache

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,27 +1,25 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
-import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
+import ListProviderAppointmentsService from '@modules/appointments/services/ListProviderAppointmentsService';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
     const { day, month, year } = request.query;
-    try {
-      const listProviderAppointments = container.resolve(
-        ListProviderAppointments,
-      );
 
-      const appointments = await listProviderAppointments.execute({
-        day: Number(day),
-        month: Number(month),
-        year: Number(year),
-        provider_id,
-      });
+    const listProviderAppointments = container.resolve(
+      ListProviderAppointmentsService,
+    );
 
-      return response.json(appointments);
-    } catch (err) {
-      return response.json({ Err: err });
-    }
+    const appointments = await listProviderAppointments.execute({
+      provider_id,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
+    });
+
+    return response.json(classToClass(appointments));
   }
 }
